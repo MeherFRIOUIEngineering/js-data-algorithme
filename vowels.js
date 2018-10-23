e705490734f23b06692c5bf358315d34b0d15226
frioui meher <meherfrioui2017@gmail.com>
2018-10-23 14:46:53 +0100

    vowels

--- vowels.js
+++ vowels.js
@@ -1,6 +1,7 @@
 function vowels(str){
   let counter=0
-  let checker='aeiou'
+  //let checker='aeiou'
+  let checker=['a','e','i','o','u']
   for(let i of str.toLowerCase()){
     if(checker.includes(i)){
       counter++
